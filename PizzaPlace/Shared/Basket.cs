cbc2d6586b8131b4ed00347c81b2c7c7d7d8601d
Игорь Пономарев <igor_p@live.ru>
2021-05-30 10:08:44 +0300

    Add project files.

---
+++ PizzaPlace/Shared/Basket.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PizzaPlace.Shared
+{
+    public class Basket
+    {
+        public Customer Customer { get; set; } = new Customer();
+        public List<int> Orders { get; set; } = new List<int>();
+        public bool HasPaid { get; set; } = false;
+    }
+}
