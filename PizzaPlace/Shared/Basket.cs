a444f3e69d5a15f990b8798240f1b59f6e59ca2f
Игорь Пономарев <igor_p@live.ru>
2021-06-23 13:12:36 +0300

    Update #1

--- PizzaPlace/Shared/Basket.cs
+++ PizzaPlace/Shared/Basket.cs
@@ -7,5 +7,15 @@ namespace PizzaPlace.Shared
         public Customer Customer { get; set; } = new Customer();
         public List<int> Orders { get; set; } = new List<int>();
         public bool HasPaid { get; set; } = false;
+
+        public void Add(int pizzaId)
+        {
+            Orders.Add(pizzaId);
+        }
+
+        public void RemoveAt(int index)
+        {
+            Orders.RemoveAt(index);
+        }
     }
 }
